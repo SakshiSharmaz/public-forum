718fd1b6f820ff72fbfeea8a41e2d9119bcc6cee
adilfaiz001 <adilfaiz01@live.com>
2022-04-14 01:54:17 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import "../src/components/css/login.css"
-// import Login from './components/screens/login';
-// import Topics from './components/screens/topics';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-// export default function indexx() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Login />}>
-//           <Route path="topics" element={<Topics />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
 
 ReactDOM.render(
-
-
-  <App />
-
-  ,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
 
 
